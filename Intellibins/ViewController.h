6b427045e11b2b32efb6836d3ec42c0c20f2e048
BenBarahona <bbaraona@gmail.com>
2015-02-20 00:29:36 -0600

    Initial Commit

---
+++ Intellibins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intellibins
+//
+//  Created by Ben on 20/2/15.
+//  Copyright (c) 2015 Intellibins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Intellibins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Intellibins
+//
+//  Created by Ben on 20/2/15.
+//  Copyright (c) 2015 Intellibins. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
